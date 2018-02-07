09a1b18dd11f33864a4df36d01430bb1da284592
Vincent Nguyen <nguyvinc@flip3.engr.oregonstate.edu>
2018-02-06 22:21:52 -0800

    Pushing tests again

--- projects/nguyvinc/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/nguyvinc/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -11,7 +11,7 @@ public class ApptTest {
      * Test that the gets methods work as expected.
      */
 	@Test
-	public void test01()  throws Throwable  {
+	public void test_apptConstructor ()  throws Throwable  {
 		int startHour=21;
 		int startMinute=30;
 		int startDay=15;
@@ -40,11 +40,99 @@ public class ApptTest {
 
 	@Test
 	public void test_isValid()  throws Throwable  {
-		int startHour = -1;
-		int startMinute = -1;
-		int startDay = -1;
-		int startMonth = -1
+		int startHour = -1;		//Invalid hour
+		int startMinute = 10;	
+		int startDay = 12;
+		int startMonth = 2;
+		int startYear = 2018;
+		String title = "Interview";
+		String description = "Interview for Tutoring Position.";
+		
+		//Create appointment with invalid hour
+		Appt appt_hour = new Appt(startHour, startMinute, startDay, startMonth, startYear, title, description);
+		
+		startHour = 10;		//Valid hour
+		startMinute = 70;	//Invalid minute
+		//Create appointment with invalid minute
+		Appt appt_minute = new Appt(startHour, startMinute, startDay, startMonth, startYear, title, description);
+		
+		startMinute = 50;	//Valid minute
+		startDay = 40;		//Invalid day
+		//Create appointment with invalid day
+		Appt appt_day = new Appt(startHour, startMinute, startDay, startMonth, startYear, title, description);
+		
+		startDay = 20;		//Valid day
+		startMonth = -1;	//Invalid month
+		//Create appointment with invalid month
+		//Creates an error
+		//Appt appt_month = new Appt(startHour, startMinute, startDay, startMonth, startYear, title, description);
+
+		assertFalse(appt_hour.getValid());
+		assertFalse(appt_minute.getValid());
+		assertFalse(appt_day.getValid());
+		//assertFalse(appt_month.getValid());
 	}
-//add more unit tests as you needed
+	
+	@Test
+	public void test_setters() throws Throwable{
+		Appt appt = new Appt(1, 1, 1, 1, 1, "Title", "Description");
+		appt.setStartHour(8);
+		appt.setStartMinute(45);
+		appt.setStartDay(23);
+		appt.setStartMonth(4);
+		appt.setStartYear(2018);
+		appt.setTitle(null);
+		appt.setDescription(null);
+		
+		assertEquals(8, appt.getStartHour());
+		assertEquals(45, appt.getStartMinute());
+		assertEquals(23, appt.getStartDay());
+		assertEquals(4, appt.getStartMonth());
+		assertEquals(2018, appt.getStartYear());
+		assertEquals("", appt.getTitle());
+		assertEquals("", appt.getDescription());
+		assertTrue(appt.getValid());
+	}
+	
+	@Test
+	public void test_setRecurrence() throws Throwable{
+		Appt appt = new Appt(1, 1, 1, 1, 1, "Title", "Description");
+		int[] recurDays = null;
+		int recurBy = 1;
+		int recurIncrement = 1;
+		int recurNumber = 2;
+		
+		appt.setRecurrence(recurDays, recurBy, recurIncrement, recurNumber);
+		assertNotNull(appt.getRecurDays());
+		assertEquals(1, appt.getRecurBy());
+		assertEquals(1, appt.getRecurIncrement());
+		assertEquals(2, appt.getRecurNumber());
+		
+		int[] recurDays2 = {1,2};
+		appt.setRecurrence(recurDays2, recurBy, recurIncrement, recurNumber);
+		assertNotNull(appt.getRecurDays());
+	}
+	
+	@Test
+	public void test_toString() throws Throwable{
+		Appt am_appt = new Appt(1, 11, 1, 1, 1, "Title", "Description");	//Valid AM appointment
+		assertEquals("\t1/1/1 at 1:11am ,Title, Description\n", am_appt.toString());
+		
+		Appt pm_appt = new Appt(14, 11, 1, 1, 1, "Title", "Description");	//Valid PM appointment
+		assertEquals("\t1/1/1 at 2:11pm ,Title, Description\n", pm_appt.toString());
+		
+		Appt midnight_appt = new Appt(0, 11, 1, 1, 1, "Title", "Description");	//Valid 12AM appointment
+		assertEquals("\t1/1/1 at 12:11am ,Title, Description\n", midnight_appt.toString());
+		
+		Appt invalid_appt = new Appt(1, -1, 1, 1, 1, "Title", "Description");	//Invalid appointment
+		assertNull(invalid_appt.toString());
+	}
+	
+	@Test
+	public void test_compartTo() throws Throwable{
+		Appt appt = new Appt(1, 11, 1, 1, 1, "Title", "Description");
+		Appt appt_2 = new Appt(2, 22, 2, 2, 2, "Title 2", "Description 2");
+		assertEquals(15, appt_2.compareTo(appt));
+	}
 	
 }
